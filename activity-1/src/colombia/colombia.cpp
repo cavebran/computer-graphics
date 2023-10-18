1e62f43ca44ca5af57b3bab236069df870eeac62
cavebran <guilherme.cavequia@gmail.com>
2023-10-17 23:28:36 -0300

    activities folders organization

---
+++ activity-1/src/colombia/colombia.cpp
@@ -0,0 +1,28 @@
+#include "../../lib/headers/Image.hpp"
+#include "../../lib/headers/Pixel.hpp"
+#include <iostream>
+
+int main() {
+	int imageWidth = 900;
+	int imageHeight = 600;
+
+	Image colombia(imageWidth, imageHeight);
+
+	int flagTwoOfFour = (int) ((imageHeight * 2) / 4);
+	int flagThreeOfFour = (int) ((imageHeight * 3) / 4);
+
+	for (int i = 0; i < imageWidth; i++) {
+		for (int j = 0; j < imageHeight; j++) {
+
+			if(j >= 0 && j < flagTwoOfFour) {
+				colombia.setPixel(j, i, Pixel(255, 205, 0));
+			} else if(j >= flagTwoOfFour && j < flagThreeOfFour) {
+				colombia.setPixel(j, i, Pixel(0, 48, 135));
+			} else {
+				colombia.setPixel(j, i, Pixel(200, 16, 46));
+			}
+		}
+	}
+
+	colombia.saveToPPM();
+}
---
+++ activity-1/src/france/france.cpp
@@ -0,0 +1,27 @@
+#include "../../lib/headers/Image.hpp"
+#include "../../lib/headers/Pixel.hpp"
+
+int main() {
+	int imageWidth = 900;
+	int imageHeight = 600;
+
+	Image france(imageWidth, imageHeight);
+
+	int flagOneOfThree = (int) (imageWidth / 3);
+	int flagTwoOfThree = (int) ((imageWidth * 2) / 3);
+
+	for (int i = 0; i < imageWidth; i++) {
+		for (int j = 0; j < imageHeight; j++) {
+
+			if(i >= 0 && i < flagOneOfThree) {
+				france.setPixel(j, i, Pixel(0, 85, 164));
+			} else if(i >= flagOneOfThree && i < flagTwoOfThree) {
+				france.setPixel(j, i, Pixel(255, 255, 255));
+			} else {
+				france.setPixel(j, i, Pixel(239, 65, 53));
+			}
+		}
+	}
+
+	france.saveToPPM();
+}
